0bba1f291e12d35b3889f1676d5747eaa357beb0
bkhanUVA <bmk4xg@virginia.edu>
2020-09-26 17:13:55 -0400

    removed uncessary data pull

--- bash/pull_imdb_data.sh
+++ bash/pull_imdb_data.sh
@@ -2,20 +2,19 @@
 
 # By bkhanUVA - https://github.com/bkhanUVA
 
-# Sample Command to pull all data: ./pull_imdb_data.sh -o ~/Desktop -mdr
+# Sample Command to pull all data: ./pull_imdb_data.sh -o ~/Desktop -mr
 
 # Defaults
 out_dir=imdbmovie/data
 imdb_api=https://datasets.imdbws.com
 
 
-while getopts i:o:mdr flag
+while getopts i:o:mr flag
 do
     case "${flag}" in
         i) imdb_api=${OPTARG};;
         o) out_dir=${OPTARG};;
-        m) pull_movies=T;;
-        d) pull_meta=T;;
+        m) pull_meta=T;;
         r) pull_rating=T;;
     esac
 done
@@ -37,6 +36,5 @@ pull_data () {
 
 
 echo "WARNING: this script pulls and uzips several large files"
-pull_data "$pull_movies" "movies" "title.akas.tsv.gz"
 pull_data "$pull_meta" "movies_metadata" "title.basics.tsv.gz"
 pull_data "$pull_rating" "movies_ratings" "title.ratings.tsv.gz"
